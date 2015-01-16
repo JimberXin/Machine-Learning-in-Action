86b19b437537599db933812a35711d2e5c602ad4
Junbo Xin <jimberxin@gmail.com>
2015-01-17 01:41:34 +0800

    add Decision Tress source code

---
+++ ch03 Decision Tree/testDecisionTrees.py
@@ -0,0 +1,8 @@
+import trees
+
+if __name__ == '__main__':
+    data_set, labels = trees.create_data_set()
+    entropy = trees.calc_shannon_entropy(data_set)
+    ret = trees.split_data_set(data_set, 1, 1)
+    feature = trees.choose_features(data_set)
+    print feature
