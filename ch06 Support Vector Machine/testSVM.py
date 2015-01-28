0f6f86c63cde0f6a6d84c63bcda21e2a120b5567
Junbo Xin <jimberxin@gmail.com>
2015-01-29 00:14:42 +0800

    update testSVM

--- ch06 Support Vector Machine/testSVM.py
+++ ch06 Support Vector Machine/testSVM.py
@@ -7,6 +7,7 @@
 import basic
 import SVM_Platt
 from numpy import *
+import classifyDigits
 
 
 def test_load():
@@ -16,25 +17,33 @@ def test_load():
 
 def test_smo_basic():
     data_arr, label_arr = basic.load_data_set('testSet.txt')
-    b, alphas = basic.smo_basic(data_arr, label_arr, 0.6, 0.001, 40)
-    print alphas[alphas>0]
+    svm = basic.smo_basic(data_arr, label_arr, 0.6, 0.001, 40)
+    print svm.alphas[svm.alphas > 0]
 
 
 def test_smo_platt():
     data_arr, label_arr = basic.load_data_set('testSet.txt')
-    b, alphas = SVM_Platt.smo_platt(data_arr, label_arr, 0.6, 0.001, 40)
-    print alphas[alphas>0]
+    svm = SVM_Platt.train_svm(data_arr, label_arr, 0.6, 0.001, 40)
+    print svm.alphas[svm.alphas>0]
 
 
-def test_rbf():
-    SVM_Platt.test_rbf()
+def test_svm():
+    data_arr, label_arr = basic.load_data_set('testSet.txt')
+    svm = SVM_Platt.train_svm(data_arr, label_arr, 200, 0.001, 10000, ('lin', 0))
+    # test_x, test_y = basic.load_data_set('testSetRBF2.txt')
+    SVM_Platt.test_svm(svm, data_arr, label_arr)
+    SVM_Platt.show_svm(svm)
+
+
+def test_digits():
+    classifyDigits.test_digits_svm()
 
 
 def main():
     # test_smo_basic()
     # test_smo_platt()
-    test_rbf()
-
+    test_svm()
+    # test_digits()
 
 if __name__ == '__main__':
     main()
