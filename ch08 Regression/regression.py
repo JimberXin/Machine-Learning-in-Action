01ae996a2797b8737f763f0e5b7c0851ab5d4a1a
Junbo Xin <jimberxin@gmail.com>
2015-02-05 19:32:47 +0800

    add local weight LR

--- ch08 Regression/regression.py
+++ ch08 Regression/regression.py
@@ -1,3 +1,9 @@
+# =================================================================================================
+# @Author: Junbo Xin
+# @Date: 2015/02/04-05
+# @Description: Simple Regression
+# =================================================================================================
+
 from numpy import *
 
 
@@ -19,16 +25,17 @@ def load_data_set(file_name):
 
 # for y = w^T * x,  w = (X^T*X)^-1 * X^T * y
 def calc_weight(x_arr, y_arr):
-    x_mat = mat(x_arr)
-    y_mat = mat(y_arr).T
-    x = x_mat.T * x_mat
+    x_mat = mat(x_arr)      # m*n matrix
+    y_mat = mat(y_arr).T    # m*1 matrix
+    x = x_mat.T * x_mat     # n*n
     if linalg.det(x) == 0.0:
         print 'matrix is singular, cannot inverse'
         return
-    ws = x.I * (x_mat.T * y_mat)
+    ws = x.I * (x_mat.T * y_mat)  # (n*n) * (n*m * m*1) = n*1
     return ws
 
 
+# giving the data set, plot the scatter figure and the estimate regression line y_hat = w * x
 def plot_line():
     import matplotlib.pyplot as plt
     # obtain the weight: w
@@ -53,6 +60,50 @@ def plot_line():
     y_hat = x_mat * w  # here y_hat is matrix, different from the previous array
     print corrcoef(y_hat.T, y_mat)
 
+    plt.show()
+
+
+#  w_hat = (X^T * W * X)* X^T * W * y
+def local_weight_point_test(test_point, x_arr, y_arr, k=1.0):
+    x_mat = mat(x_arr)
+    y_mat = mat(y_arr).T
+    m, n = shape(x_mat)
+    weights = mat(eye((m)))
+    for i in range(m):
+        diff_mat = test_point - x_mat[i, :]  # difference between test_point and current point( i in 0--m)
+        weights[i, i] = exp(diff_mat*diff_mat.T/(-2.0*k**2))
+    x = x_mat.T * (weights*x_mat)
+    if linalg.det(x) == 0.0:
+        print 'This matrix is singular, cannot do inverses'
+        return
+    ws = x.I * x_mat.T * weights * y_mat
+    return test_point * ws
+
+
+def local_weight_array_test(test_arr, x_arr, y_arr, k=1.0):
+    m, n = shape(x_arr)
+    y_hat = zeros(m)
+    for i in range(m):
+        y_hat[i] = local_weight_point_test(test_arr[i], x_arr, y_arr, k)
+    return y_hat
+
+
+def plot_line_lw():
+    import matplotlib.pyplot as plt
+    x_arr, y_arr = load_data_set('ex0.txt')
+    y_hat = local_weight_array_test(x_arr, x_arr, y_arr, k=3)
+    x_mat = mat(x_arr)
+    y_mat = mat(y_arr)
+    sorted_index = x_mat[:, 1].argsort(0)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    x_copy = x_mat.copy()
+    x_copy.sort(0)
+    ax.scatter(array(x_mat[:, 1]), array(y_mat.T[:, 0]), c='red')
+    ax.plot(array(x_copy[:, 1]), array(y_hat[sorted_index]))
+    ax.plot()
     plt.show()
 
 
