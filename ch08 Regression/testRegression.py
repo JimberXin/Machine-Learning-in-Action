f701dccb81efb81ba2884bd2fa6653dc7d340f94
Junbo Xin <jimberxin@gmail.com>
2015-02-06 12:12:19 +0800

    update regression

--- ch08 Regression/testRegression.py
+++ ch08 Regression/testRegression.py
@@ -1,4 +1,10 @@
-from numpy import *
+# =================================================================================================
+# @Author: Junbo Xin
+# @Date: 2015/02/04-06
+# @Description: Test file for Regression
+# =================================================================================================
+
+
 import regression
 
 
@@ -12,6 +18,11 @@ def test_plot():
     regression.plot_line()
 
 
+def test_plot_weight():
+    x_arr, y_arr = regression.load_data_set('ex0.txt')
+    regression.plot_local_weight([1.0, 0.5], x_arr, k=0.1)
+
+
 def test_local_lr():
     regression.plot_line_lw()
 
@@ -20,6 +31,7 @@ def main():
     # test_basic()
     # test_plot()
     test_local_lr()
+    # test_plot_weight()
 
 
 if __name__ == '__main__':
