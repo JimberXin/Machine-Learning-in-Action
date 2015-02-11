b1eca8888042b704688049f408714935b4fb6bee
Junbo Xin <jimberxin@gmail.com>
2015-02-11 22:58:43 +0800

    update regression Tree

--- ch09 Tree Regression/testRegTree.py
+++ ch09 Tree Regression/testRegTree.py
@@ -24,13 +24,28 @@ def test_create_tree():
 
 
 def test_plot():
-    regressionTree.plot_data_set('ex0.txt')
+    regressionTree.plot_data_set('ex00.txt')
+
+
+def test_prune():
+    # training a tree
+    data_arr = regressionTree.load_data_set('ex2.txt')
+    training_mat = mat(data_arr)
+    my_tree = regressionTree.create_tree(training_mat, ops=(0, 1))
+
+    # test using the tree
+    test_set = regressionTree.load_data_set('ex2test.txt')
+    test_mat = mat(test_set)
+    tree = regressionTree.prune(my_tree, test_mat)
+    print tree
 
 
 def main():
     # test_basic()
     # test_create_tree()
-    test_plot()
+    # test_plot()
+    tree = test_prune()
+    print tree
 
 
 if __name__ == '__main__':
