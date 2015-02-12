955b18a7ba7942af4e44949f1e0e60e5d17265c9
Junbo Xin <jimberxin@gmail.com>
2015-02-13 00:05:12 +0800

    add kMeans

---
+++ ch10 K-Means/testKMeans.py
@@ -0,0 +1,37 @@
+# ===============================================================================================
+# Author: Junbo Xin
+# Date: 2015/02/12
+# Description:  Test file for K Means
+# ===============================================================================================
+import kMeans
+from numpy import *
+
+
+def test_load():
+    data_mat = mat(kMeans.load_data_set('testSet.txt'))
+    print min(data_mat[:, 0])
+    print max(data_mat[:, 1])
+    print kMeans.rand_center(data_mat, 2)
+
+
+def test_k_means():
+    k = 4
+    data_mat = mat(kMeans.load_data_set('testSet.txt'))
+    center, cluster = kMeans.k_means(data_mat, k)
+    kMeans.plot_k_means(data_mat, cluster, center, k)
+
+
+def test_binary_k_means():
+    k = 3
+    data_mat = mat(kMeans.load_data_set('testSet2.txt'))
+    center, cluster = kMeans.binary_k_means(data_mat, k)
+    kMeans.plot_k_means(data_mat, cluster, center, k)
+
+def main():
+    # test_load()
+    test_k_means()
+    # test_binary_k_means()
+
+
+if __name__ == '__main__':
+    main()
