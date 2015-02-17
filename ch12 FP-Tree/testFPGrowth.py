a3e71e393d95585bddb0e4b182e00c997e1abe86
Junbo Xin <jimberxin@gmail.com>
2015-02-18 00:20:24 +0800

    update FP tree

--- ch12 FP-Tree/testFPGrowth.py
+++ ch12 FP-Tree/testFPGrowth.py
@@ -8,14 +8,30 @@ from numpy import *
 import fpGrowth
 
 
+def test_tree_node():
+    root = fpGrowth.TreeNode('pyramid', 9, None)
+    root.children['eye'] = fpGrowth.TreeNode('eye', 13, None)
+    root.display()
+    root.children['phoenix'] = fpGrowth.TreeNode('phoenix', 3, None)
+    root.display()
+
+
 def test_simple():
     data_set = fpGrowth.load_simple_data()
-    dic = fpGrowth.create_tree(data_set)
+    dic = fpGrowth.create_init_set(data_set)
     print dic
 
 
+def test_create_tree():
+    data_set = fpGrowth.load_simple_data()
+    init_set = fpGrowth.create_init_set(data_set)
+    tree, head_table = fpGrowth.create_tree(init_set, 3)
+
+
 def main():
-    test_simple()
+    # test_simple()
+    # test_tree_node()
+    test_create_tree()
 
 
 if __name__ == '__main__':
